922556f3e79ae34a9de6185725ce8cc0285641fb
cdenaro <Viaggiare04>
2016-09-21 17:10:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# shortest_api 
+shorte.st api wrapper
+
+```php
+$KEY      = 'YOUR-KEY';
+$shortest = new shortest($KEY);
+$link     = $shortest->__get('http://www.google.com');
+```
---
+++ src/shortest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace shortest\Api;
+
+/**
+ * Class shortest 
+ *
+ * @package shortest\Api
+ */
+class shortest {
+  private $KEY;
+  private $URL = 'https://api.shorte.st/v1/data/url';
+  /**
+   * Constructor
+   *
+   * @param string $KEY Api KEY
+   */
+  public function __construct($key)
+  {
+    $this->KEY  = $key;
+  }
+  /**
+   * __get method
+   *
+   * @param string $url
+   *
+   * @return string|boolean
+   */
+  public function __get($url)
+  {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $this->URL); 
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'public-api-token: '.$this->KEY
+    ));
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+    curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query(array(
+      'urlToShorten' => $url
+    )));
+
+    $response = curl_exec($ch);
+    if ($response) {
+      return $response;
+    }
+    return false;
+  }
+}
