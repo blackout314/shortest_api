7149bf4ed07200f5633a068590320995e8f62883
Carlo Denaro <blackout_lair@yahoo.com>
2018-08-22 09:19:14 +0200

    Update shortest.php

--- src/shortest.php
+++ src/shortest.php
@@ -29,18 +29,20 @@ class shortest {
   public function __get($url)
   {
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $this->URL); 
+    curl_setopt($ch, CURLOPT_URL, $this->URL);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
       'public-api-token: '.$this->KEY
     ));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
     curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query(array(
       'urlToShorten' => $url
     )));
 
     $response = curl_exec($ch);
-    if ($response) {
-      return $response;
+    $json = json_decode($response);
+    if ($json->status === 'ok') {
+      return $json->shortenedUrl;
     }
     return false;
   }
